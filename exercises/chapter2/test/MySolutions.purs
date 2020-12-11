f6d06cde622bcb73e6f3d0a706de756d9b39cbd2
howard chang <idhowardgj94@gmail.com>
2020-12-11 23:40:27 +0800

    finish chapter2

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,13 @@
 module Test.MySolutions where
 
 import Prelude
+import Math (sqrt, pi, e)
+import Global
+
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea radis = radis * radis * pi
+
+--  readFloat :: String -> Number
+
+addE s = e + readFloat s
